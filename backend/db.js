ca09b6f8b8427ead39d2b40d0e4d959c5cf51db9
Zettervaall <moa.zettervall@gmail.com>
2025-05-24 17:35:28 +0200

    database

--- backend/db.js
+++ backend/db.js
@@ -21,6 +21,17 @@ database.serialize(() => {
       FOREIGN KEY (category_id) REFERENCES categories(id)
     )
   `);
+
+const stmt = database.prepare(`
+    INSERT INTO flowers (name, description, price, imageUrl)
+    VALUES (?, ?, ?, ?)
+  `);
+
+  stmt.run('Ros', 'Röd ros i kruka', 59, 'https://url-till-bild.av/ros.jpg');
+  stmt.run('Tulpan', 'Bukett med blandade tulpaner', 89, 'https://url-till-bild.av/tulpan.jpg');
+  stmt.run('Lilja', 'Vit lilja med doft', 69, 'https://url-till-bild.av/lilja.jpg');
+
+  stmt.finalize();
 });
 
 module.exports = database;
