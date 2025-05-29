4475d842e728b4662eb4f802709cee9996072d10
nudemfisli <nudemfisli@gmail.com>
2025-05-29 13:07:42 +0200

    Finished navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,22 +4,27 @@ import './Navigation.css';
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <div className="navbar-content">
-                <div className="nav-links">
-                    <a href="/flowers">FLOWERS</a>
-                    <a href="/decoration">DECORATION</a>
-                    <a href="/gift-ideas">GIFT IDEAS</a>
-                    <a href="/tips">TIPS</a>
-                    <a href="/contact">CONTACT</a>
+        <>
+            <nav className="navbar">
+                <div className="navbar-content">
+                    <div className="nav-links">
+                        <a href="/flowers">FLOWERS</a>
+                        <a href="/decoration">DECORATION</a>
+                        <a href="/gift-ideas">GIFT IDEAS</a>
+                        <a href="/tips">TIPS</a>
+                        <a href="/contact">CONTACT</a>
+                    </div>
                 </div>
-                <div className="nav-icons">
-                    <FaUser />
-                    <FaSearch />
-                    <FaShoppingCart />
-                </div>
+            </nav>
+
+            <div className="nav-icons">
+                <FaUser />
+                <FaSearch />
+                <FaShoppingCart />
             </div>
-        </nav>
+
+            <div className="break-line"></div>
+        </>
     );
 };
 
