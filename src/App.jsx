8a7670a8a593a194e97f5ebeaa56f7e7aa952ca2
nudemfisli <nudemfisli@gmail.com>
2025-05-29 12:40:35 +0200

    Started on navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './App.css';
 import Products from './components/Products';
 
 import Header from './components/Header';
+import Navigation from './components/Navigation';
 
 function App() {
     return (
         <>
             <Header />
+            <Navigation />
             <h1>hejsan</h1>
             <Products />
         </>
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FaUser, FaSearch, FaShoppingCart } from 'react-icons/fa';
+import './Navigation.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-content">
+                <div className="nav-links">
+                    <a href="/flowers">FLOWERS</a>
+                    <a href="/decoration">DECORATION</a>
+                    <a href="/gift-ideas">GIFT IDEAS</a>
+                    <a href="/tips">TIPS</a>
+                    <a href="/contact">CONTACT</a>
+                </div>
+                <div className="nav-icons">
+                    <FaUser />
+                    <FaSearch />
+                    <FaShoppingCart />
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
