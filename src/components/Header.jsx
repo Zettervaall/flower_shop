15cd7412f4fd6b26adfd2bfc025087e94e31871c
Zettervaall <moa.zettervall@gmail.com>
2025-05-30 22:14:58 +0200

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,21 +2,40 @@ import './Header.css';
 
 function Header() {
     return (
-        <header className="header">
-            <div className='navbar_logo'>RR</div>
-            <div className="header_content">
-                <div className="img_poeny">
-                    <img src="src\assets\1748529422710zt4r3ky6.webp" alt="" />
+        <>
+            <header className="header">
+                <div className="navbar_logo">
+                    <p>
+                        R<span className="R">R</span>
+                    </p>
                 </div>
-                <div className="logo">
+                <div className="header_text">
                     <h1>
-                        <span>ROSE</span>
-                        <br />
-                        <span>& RUE</span>
+                        BLOOM<span className="white-text">ING</span>
                     </h1>
+                    <p>
+
+                        Fresh flow<span className="white-text">ers, handpicked with</span> love – brin<span className="white-text">ging beauty, color, and</span> calm into <span className="white-text">your everyday life.</span>
+                    </p>
+                    <div className="color_block"></div>
                 </div>
-            </div>
-        </header>
+                <div className="header_content">
+                    <div className="img_poeny">
+                        <img
+                            src="src\assets\1748529422710zt4r3ky6.webp"
+                            alt=""
+                        />
+                    </div>
+                    <div className="logo">
+                        <h1>
+                            <span>ROSE</span>
+                            <br />
+                            <span>& RUE</span>
+                        </h1>
+                    </div>
+                </div>
+            </header>
+        </>
     );
 }
 
