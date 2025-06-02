74d493c1135014d73fd87a15ca4aac1d39ff2139
nudemfisli <nudemfisli@gmail.com>
2025-06-02 18:29:10 +0200

    Updated styling header and navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
@@ -5,34 +6,46 @@ function Header() {
         <>
             <header className="header">
                 <div className="navbar_logo">
-                    <p>
-                        R<span className="R">R</span>
-                    </p>
+                    <Link
+                        to="/"
+                        className="navbar_logo_link"
+                        aria-label="Go to homepage"
+                    >
+                        <p>
+                            R<span className="R">R</span>
+                        </p>
+                    </Link>
                 </div>
                 <div className="header_text">
                     <h1>
                         BLOOM<span className="white-text">ING</span>
                     </h1>
                     <p>
-
-                        Fresh flow<span className="white-text">ers, handpicked with</span> love – brin<span className="white-text">ging beauty, color, and</span> calm into <span className="white-text">your everyday life.</span>
+                        Fresh flow
+                        <span className="white-text">
+                            ers, handpicked with
+                        </span>{' '}
+                        love – brin
+                        <span className="white-text">
+                            ging beauty, color, and
+                        </span>{' '}
+                        calm into{' '}
+                        <span className="white-text">your everyday life.</span>
                     </p>
                     <div className="color_block"></div>
                 </div>
                 <div className="header_content">
-                    {/* <div className="img_poeny">
-                        <img
-                            src="src\assets\1748529422710zt4r3ky6.webp"
-                            alt=""
-                        />
-                    </div> */}
-                    <div className="logo">
-                        <h1>
-                            <span className='rose'>ROSE</span>
-                            <br />
-                            <span className='rue'>& Rue</span>
-                        </h1>
-                    </div>
+                    <Link to="/">
+                        <div className="image-header"></div>
+                    </Link>
+                </div>
+
+                <div className="logo">
+                    <h1>
+                        <span className="rose">ROSE</span>
+                        <br />
+                        <span className="rue">& Rue</span>
+                    </h1>
                 </div>
             </header>
         </>
