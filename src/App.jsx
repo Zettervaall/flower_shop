775ef3d73bb0c0dce8ae051d89976fd8595a9c1b
nudemfisli <nudemfisli@gmail.com>
2025-06-02 21:20:58 +0200

    Second section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
 import FirstSection from './components/FirstSection';
+
 import ThirdSection from './components/ThirdSection';
 import AboutSection from './components/AboutSection';
 import CarouselSection from './components/CarouselSection';
